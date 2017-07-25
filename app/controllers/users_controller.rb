91a312c0e71a5a7257a71c82aeffc4440f626cd2
Minoryo <ryotafoot@me.com>
2017-07-26 01:03:26 +0900

    簡易的なユーザー登録機能完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,21 @@ class UsersController < ApplicationController
   end
 
   def show
-    @id = params[:id]
+    @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: "ユーザ登録完了"
+    else
+      render :new
+    end
   end
 
   private
