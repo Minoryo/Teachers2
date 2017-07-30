587b2d3d7765d0faa95c13806998b18bf723a632
Minoryo <ryotafoot@me.com>
2017-07-30 15:43:40 +0900

    データベースのremember_tokenがnilになる。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user, notice: "ユーザ登録完了"
+      redirect_to login_path
     else
       render action: :new
     end
